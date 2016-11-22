e448ccd997bc1754a209d103f8d7b2e07d00badc
Christopher <Christopher.Fenner@me.com>
2016-11-22 15:29:22 +0100

    fix typo

--- netatmo.js
+++ netatmo.js
@@ -166,12 +166,12 @@ Module.register('netatmo', {
         case this.moduleType.MAIN:
         case this.moduleType.INDOOR:
           result += $('<div/>').css({
-            width: '.2em',
-            height: '.2em',
-            background-color: 'limegreen',
-            border-radius: '5em',
-            box-shadow: '0 0 1em 1.8em limegreen',
-            margin: '2.4em;'
+            'width': '.2em',
+            'height': '.2em',
+            'background-color': 'limegreen',
+            'border-radius': '5em',
+            'box-shadow': '0 0 1em 1.8em limegreen',
+            'margin': '2.4em;'
           })[0].outerHTML;
           result += $('<div/>').addClass('small').append(module.dashboard_data['CO2'] + ' ppm')[0].outerHTML;
           break;
