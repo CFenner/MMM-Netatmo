d112b63dcb9355b9ae239bb651c8c45b51aeb51d
Christopher Fenner <christopher.fenner@me.com>
2022-01-22 13:17:21 +0100

    fix eslint findings

--- netatmo.js
+++ netatmo.js
@@ -4,8 +4,8 @@
  * By Christopher Fenner http://github.com/CFenner
  * MIT Licensed.
  */
- /* global Module, Log */
- Module.register('netatmo', {
+/* global Module, Log */
+Module.register('netatmo', {
   // default config
   defaults: {
     initialDelay: 0,
@@ -26,20 +26,20 @@
     fontClassModuleName: 'xsmall',
     fontClassPrimary: 'large',
     fontClassSecondary: 'xsmall',
-    fontClassMeasurement: 'xsmall',
+    fontClassMeasurement: 'xsmall'
   },
   notifications: {
-    'auth': 'NETATMO_AUTH',
-    'auth_response': 'NETATMO_AUTH_RESPONSE',
-    'data': 'NETATMO_DATA',
-    'data_response': 'NETATMO_DATA_RESPONSE',
+    auth: 'NETATMO_AUTH',
+    auth_response: 'NETATMO_AUTH_RESPONSE',
+    data: 'NETATMO_DATA',
+    data_response: 'NETATMO_DATA_RESPONSE'
   },
   moduleType: {
-    MAIN: "NAMain",
-    INDOOR: "NAModule4",
-    OUTDOOR: "NAModule1",
-    RAIN: "NAModule3",
-    WIND: "NAModule2"
+    MAIN: 'NAMain',
+    INDOOR: 'NAModule4',
+    OUTDOOR: 'NAModule1',
+    RAIN: 'NAModule3',
+    WIND: 'NAModule2'
   },
   measurement: {
     CO2: 'CO2',
@@ -55,25 +55,25 @@
     GUST_ANGLE: 'GustAngle',
     RAIN: 'Rain',
     RAIN_PER_HOUR: 'sum_rain_1',
-    RAIN_PER_DAY: 'sum_rain_24',
+    RAIN_PER_DAY: 'sum_rain_24'
   },
   // init method
   start: function () {
-    let self = this
+    const self = this
     Log.info(`Starting module: ${this.name}`)
     self.loaded = false
     self.moduleList = []
 
-		//get a new token at start-up. When receive, GET_CAMERA_EVENTS will be requested
-    setTimeout(function() {
-			self.sendSocketNotification(self.notifications.data, self.config);
-		}, this.config.initialDelay *1000);
+    // get a new token at start-up. When receive, GET_CAMERA_EVENTS will be requested
+    setTimeout(function () {
+      self.sendSocketNotification(self.notifications.data, self.config)
+    }, this.config.initialDelay * 1000)
 
-		//set auto-update
-    setInterval( function () {
-			//request directly the data, with the previous token. When the token will become invalid (error 403), it will be requested again
-			self.sendSocketNotification(self.notifications.data, self.config);
-		}, this.config.updateInterval * 60 * 1000 + this.config.initialDelay *1000);
+    // set auto-update
+    setInterval(function () {
+      // request directly the data, with the previous token. When the token will become invalid (error 403), it will be requested again
+      self.sendSocketNotification(self.notifications.data, self.config)
+    }, this.config.updateInterval * 60 * 1000 + this.config.initialDelay * 1000)
   },
   updateModuleList: function (station) {
     const moduleList = []
@@ -84,23 +84,22 @@
       moduleList.push(this.getModule(module))
     }.bind(this))
 
-    if (station.reachable)
-      this.lastUpdate = station.dashboard_data.time_utc;
+    if (station.reachable) { this.lastUpdate = station.dashboard_data.time_utc }
     this.loaded = true
     if (JSON.stringify(this.moduleList) === JSON.stringify(moduleList)) {
       return
     }
     this.moduleList = moduleList
   },
-  getModule: function(module){
-    let result = {}
+  getModule: function (module) {
+    const result = {}
 
     result.name = module.module_name
     result.measurementList = []
 
     if (!module.reachable) return result
 
-//TODO check module.reachable
+    // TODO check module.reachable
     let primaryType = ''
     let primaryValue = ''
     let secondaryType = ''
@@ -114,43 +113,43 @@
         result.measurementList.push(this.getMeasurement(module, this.measurement.NOISE))
         // break; fallthrough
       case this.moduleType.INDOOR:
-        secondaryType = this.measurement.CO2;
-        secondaryValue = module.dashboard_data[secondaryType];
-        let status = 'good';
-        if(secondaryValue > 800) status = 'bad'
-        if(secondaryValue > 1600) status = 'bad'
-        result.secondary = {visualClass: status, value: this.getValue(secondaryType, secondaryValue), class: this.kebabCase(secondaryType)}
+        secondaryType = this.measurement.CO2
+        secondaryValue = module.dashboard_data[secondaryType]
+        let status = 'good'
+        if (secondaryValue > 800) status = 'bad'
+        if (secondaryValue > 1600) status = 'bad'
+        result.secondary = { visualClass: status, value: this.getValue(secondaryType, secondaryValue), class: this.kebabCase(secondaryType) }
         // break; fallthrough
       case this.moduleType.OUTDOOR:
         primaryType = this.measurement.TEMPERATURE
-        primaryValue = module.dashboard_data?module.dashboard_data[primaryType]:''
-        result.primary = {unit: '', value: this.getValue(primaryType, primaryValue), class: this.kebabCase(primaryType)}
+        primaryValue = module.dashboard_data ? module.dashboard_data[primaryType] : ''
+        result.primary = { unit: '', value: this.getValue(primaryType, primaryValue), class: this.kebabCase(primaryType) }
         result.measurementList.push(this.getMeasurement(module, this.measurement.TEMPERATURE_TREND))
         result.measurementList.push(this.getMeasurement(module, this.measurement.HUMIDITY))
-        break;
+        break
       case this.moduleType.WIND:
         primaryType = this.measurement.WIND_STRENGTH
-        primaryValue = module.dashboard_data?module.dashboard_data[primaryType]:''
-        result.primary = {unit: 'm/s', value: primaryValue, class: this.kebabCase(primaryType)}
+        primaryValue = module.dashboard_data ? module.dashboard_data[primaryType] : ''
+        result.primary = { unit: 'm/s', value: primaryValue, class: this.kebabCase(primaryType) }
         secondaryType = this.measurement.WIND_ANGLE
-        secondaryValue = module.dashboard_data[type];
-        result.secondary = {visualClass: 'xlarge wi wi-direction-up', value: this.getValue(secondaryType, secondaryValue), class: this.kebabCase(secondaryType)}
-  //         $('<div/>').addClass('visual xlarge wi wi-direction-up').css('transform', 'rotate(' + value + 'deg)')
+        secondaryValue = module.dashboard_data[type]
+        result.secondary = { visualClass: 'xlarge wi wi-direction-up', value: this.getValue(secondaryType, secondaryValue), class: this.kebabCase(secondaryType) }
+        //         $('<div/>').addClass('visual xlarge wi wi-direction-up').css('transform', 'rotate(' + value + 'deg)')
         result.measurementList.push(this.getMeasurement(module, this.measurement.GUST_STRENGTH))
         result.measurementList.push(this.getMeasurement(module, this.measurement.GUST_ANGLE))
-        break;
+        break
       case this.moduleType.RAIN:
         primaryType = this.measurement.RAIN
-        primaryValue = module.dashboard_data?module.dashboard_data[primaryType]:''
-        result.primary = {unit: 'mm/h', value: primaryValue, class: this.kebabCase(primaryType)}
+        primaryValue = module.dashboard_data ? module.dashboard_data[primaryType] : ''
+        result.primary = { unit: 'mm/h', value: primaryValue, class: this.kebabCase(primaryType) }
         result.measurementList.push(this.getMeasurement(module, this.measurement.RAIN_PER_HOUR))
         result.measurementList.push(this.getMeasurement(module, this.measurement.RAIN_PER_DAY))
-        break;
+        break
       default:
-        break;
+        break
     }
     // add additional measurements
-    if (module.type === this.moduleType.MAIN){
+    if (module.type === this.moduleType.MAIN) {
       result.measurementList.push(this.getMeasurement(module, 'wifi', module.wifi_status))
     } else {
       result.measurementList.push(this.getMeasurement(module, 'radio', module.rf_status))
@@ -158,76 +157,75 @@
     }
     return result
   },
-  getMeasurement: function(module, measurement, value){
-    value = value?value:module.dashboard_data[measurement]
+  getMeasurement: function (module, measurement, value) {
+    value = value || module.dashboard_data[measurement]
     return {
       value: value,
       icon: this.getIcon(measurement),
-      label: this.translate(measurement.toUpperCase()),
+      label: this.translate(measurement.toUpperCase())
     }
   },
   kebabCase: string => string
-    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
     .replace(/[\s_]+/g, '-')
     .toLowerCase(),
-  getValue: function(dataType, value) {
-    if(!value)
-      return value;
+  getValue: function (dataType, value) {
+    if (!value) { return value }
     switch (dataType) {
       case this.measurement.CO2:
-        return value.toFixed(0) + ' ppm';
+        return value.toFixed(0) + ' ppm'
       case this.measurement.NOISE:
-        return value.toFixed(0) + ' dB';
+        return value.toFixed(0) + ' dB'
       case this.measurement.HUMIDITY:
       case 'Battery':
       case 'WiFi':
       case 'Radio':
-        return value.toFixed(0) + '%';
+        return value.toFixed(0) + '%'
       case this.measurement.PRESSURE:
-        return value.toFixed(0) + ' mbar';
+        return value.toFixed(0) + ' mbar'
       case this.measurement.TEMPERATURE:
-        return value.toFixed(1) + '°';
+        return value.toFixed(1) + '°'
       case this.measurement.RAIN:
       case this.measurement.RAIN_PER_HOUR:
       case this.measurement.RAIN_PER_DAY:
-        return value.toFixed(1) + ' mm/h';
+        return value.toFixed(1) + ' mm/h'
       case this.measurement.WIND_STRENGTH:
       case this.measurement.GUST_STRENGTH:
-        return value.toFixed(0) + ' m/s';
+        return value.toFixed(0) + ' m/s'
       case this.measurement.WIND_ANGLE:
       case this.measurement.GUST_ANGLE:
-        return this.direction(value) + ' | ' + value + '°';
+        return this.direction(value) + ' | ' + value + '°'
       default:
-        return value;
+        return value
     }
   },
-  getDirection: function(value){
-    if(value < 11.25) return 'N';
-    if(value < 33.75) return 'NNE';
-    if(value < 56.25) return 'NE';
-    if(value < 78.75) return 'ENE';
-    if(value < 101.25) return 'E';
-    if(value < 123.75) return 'ESE';
-    if(value < 146.25) return 'SE';
-    if(value < 168.75) return 'SSE';
-    if(value < 191.25) return 'S';
-    if(value < 213.75) return 'SSW';
-    if(value < 236.25) return 'SW';
-    if(value < 258.75) return 'WSW';
-    if(value < 281.25) return 'W';
-    if(value < 303.75) return 'WNW';
-    if(value < 326.25) return 'NW';
-    if(value < 348.75) return 'NNW';
-    return 'N';
+  getDirection: function (value) {
+    if (value < 11.25) return 'N'
+    if (value < 33.75) return 'NNE'
+    if (value < 56.25) return 'NE'
+    if (value < 78.75) return 'ENE'
+    if (value < 101.25) return 'E'
+    if (value < 123.75) return 'ESE'
+    if (value < 146.25) return 'SE'
+    if (value < 168.75) return 'SSE'
+    if (value < 191.25) return 'S'
+    if (value < 213.75) return 'SSW'
+    if (value < 236.25) return 'SW'
+    if (value < 258.75) return 'WSW'
+    if (value < 281.25) return 'W'
+    if (value < 303.75) return 'WNW'
+    if (value < 326.25) return 'NW'
+    if (value < 348.75) return 'NNW'
+    return 'N'
   },
-  getIcon: function(dataType) {
+  getIcon: function (dataType) {
     switch (dataType) {
       case this.measurement.CO2:
-        return 'fa-lungs';
+        return 'fa-lungs'
       case this.measurement.NOISE:
-        return 'fa-volume-up';
+        return 'fa-volume-up'
       case this.measurement.HUMIDITY:
-        return 'fa-tint';
+        return 'fa-tint'
       // case this.measurement.PRESSURE:
       // case this.measurement.PRESSURE:
       // case this.measurement.GUST_ANGLE:
@@ -238,15 +236,15 @@
       // return 'fa-tachometer-alt';
       case this.measurement.PRESSURE_TREND:
       case this.measurement.TEMPERATURE_TREND:
-        return 'fa-long-arrow-alt-right';
+        return 'fa-long-arrow-alt-right'
       case 'wifi':
-        return 'fa-wifi';
+        return 'fa-wifi'
       case 'radio':
-        return 'fa-broadcast-tower';
+        return 'fa-broadcast-tower'
       case 'battery':
-        return 'fa-battery-three-quarters';
+        return 'fa-battery-three-quarters'
       default:
-        return 'fa-ambulance';
+        return 'fa-ambulance'
     }
   },
   getStyles: function () {
@@ -269,10 +267,10 @@
       fontClassModuleName: this.config.fontClassModuleName,
       fontClassPrimary: this.config.fontClassPrimary,
       fontClassSecondary: this.config.fontClassSecondary,
-      fontClassMeasurement: this.config.fontClassMeasurement,
+      fontClassMeasurement: this.config.fontClassMeasurement
     }
   },
-  getTranslations: function() {
+  getTranslations: function () {
     return {
       en: 'l10n/en.json',
       de: 'l10n/de.json',
@@ -280,33 +278,33 @@
       cs: 'l10n/cs.json',
       nb: 'l10n/nb.json',
       nn: 'l10n/nn.json'
-    };
+    }
   },
   socketNotificationReceived: function (notification, payload) {
     const self = this
     Log.debug('received ' + notification)
     if (notification === self.notifications.auth_response) {
       console.log(payload)
-      if(payload.status === 'OK') {
-        this.sendSocketNotification(self.notifications.data, self.config);
+      if (payload.status === 'OK') {
+        this.sendSocketNotification(self.notifications.data, self.config)
       } else {
-        console.log("AUTH FAILED " + payload.message)
+        console.log('AUTH FAILED ' + payload.message)
       }
     } else if (notification === self.notifications.data_response) {
       console.log(payload)
 
       if (payload.status === 'OK') {
-        console.log("devices returned")
-        var station = payload.payloadReturn[0];
+        console.log('devices returned')
+        const station = payload.payloadReturn[0]
         this.updateModuleList(station)
-        this.updateDom(this.config.animationSpeed);
-      } else if(payload.status === 'INVALID_TOKEN') {
+        this.updateDom(this.config.animationSpeed)
+      } else if (payload.status === 'INVALID_TOKEN') {
         // node_module has no valid token, reauthenticate
-        console.log("DATA FAILED, refreshing token")
-        this.sendSocketNotification(self.notifications.auth, self.config);
+        console.log('DATA FAILED, refreshing token')
+        this.sendSocketNotification(self.notifications.auth, self.config)
       } else {
-        console.log("DATA FAILED " + payload.message)
+        console.log('DATA FAILED ' + payload.message)
       }
     }
   }
-});
+})
--- node_helper.js
+++ node_helper.js
@@ -5,152 +5,152 @@
  * MIT Licensed.
  */
 const NodeHelper = require('node_helper')
-const https = require("https")
+const https = require('https')
 const URLSearchParams = require('@ungap/url-search-params')
 
 module.exports = NodeHelper.create({
   start: function () {
     console.log('Netatmo helper started ...')
-    this.token = null;
-	  this.token_time = null;
+    this.token = null
+	  this.token_time = null
   },
   notifications: {
-    'auth': 'NETATMO_AUTH',
-    'auth_response': 'NETATMO_AUTH_RESPONSE',
-    'data': 'NETATMO_DATA',
-    'data_response': 'NETATMO_DATA_RESPONSE',
+    auth: 'NETATMO_AUTH',
+    auth_response: 'NETATMO_AUTH_RESPONSE',
+    data: 'NETATMO_DATA',
+    data_response: 'NETATMO_DATA_RESPONSE'
   },
-  authenticate: function(config) {
+  authenticate: function (config) {
     const self = this
     self.config = config
 
-    //TODO: only update if token is invalid
+    // TODO: only update if token is invalid
 
-    let req = https.request({
+    const req = https.request({
       hostname: self.config.apiBase,
       path: self.config.authEndpoint,
       method: 'POST',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-    }, self.callbackAuthenticate.bind(self));
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    }, self.callbackAuthenticate.bind(self))
 
-    req.on('error', function(e) {
-      console.log('There is a problem with your request:', e.message);
+    req.on('error', function (e) {
+      console.log('There is a problem with your request:', e.message)
       self.sendSocketNotification(self.notifications.auth_response, {
         // instanceID: self.config.instanceID,
-        payloadReturn: e.message,
-      });
-    });
+        payloadReturn: e.message
+      })
+    })
 
     req.write(new URLSearchParams({
-      'scope': "read_station",
-      'grant_type': "password",
-      'username': self.config.username,
-      'password': self.config.password,
-      'client_id':self.config.clientId,
-      'client_secret': self.config.clientSecret,
-    }).toString());
+      scope: 'read_station',
+      grant_type: 'password',
+      username: self.config.username,
+      password: self.config.password,
+      client_id: self.config.clientId,
+      client_secret: self.config.clientSecret
+    }).toString())
 
-    req.end();
+    req.end()
   },
-  loadData: function(config) {
+  loadData: function (config) {
     const self = this
     self.config = config
 
-    let req = https.request({
+    const req = https.request({
       hostname: self.config.apiBase,
       path: self.config.dataEndpoint,
       // method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${self.token}`,
-      },
-    }, self.callbackData.bind(self));
+        Authorization: `Bearer ${self.token}`
+      }
+    }, self.callbackData.bind(self))
 
-    req.on('error', function(e) {
-      console.log('There is a problem with your request:', e.message);
+    req.on('error', function (e) {
+      console.log('There is a problem with your request:', e.message)
       self.sendSocketNotification(self.notifications.data_response, {
         payloadReturn: e.message,
         status: 'NOTOK',
-        message: e.message,
-      });
-    });
-/*
+        message: e.message
+      })
+    })
+    /*
     req.write(new URLSearchParams({
       'access_token': self.token,
     }).toString());
 */
 
-    req.end();
+    req.end()
   },
-  callbackAuthenticate: function(response) {
+  callbackAuthenticate: function (response) {
     const self = this
-    let result = '';
+    let result = ''
 
-    response.on('error', function(e) {console.log('error', e)})
-    response.on('data', function (chunk) {result += chunk})
+    response.on('error', function (e) { console.log('error', e) })
+    response.on('data', function (chunk) { result += chunk })
     response.on('end', function () {
-      result = JSON.parse(result);
+      result = JSON.parse(result)
       if (response.statusCode == '200') {
-        console.log("UPDATING TOKEN " + result.access_token)
+        console.log('UPDATING TOKEN ' + result.access_token)
         self.token = result.access_token
-        self.token_time = new Date();
-        //we got a new token, save it to main file to allow it to request the datas
+        self.token_time = new Date()
+        // we got a new token, save it to main file to allow it to request the datas
         self.sendSocketNotification(self.notifications.auth_response, {
-          status: 'OK',
+          status: 'OK'
         })
       } else {
-        console.log('status code:', response.statusCode, '\n', result);
+        console.log('status code:', response.statusCode, '\n', result)
         self.sendSocketNotification(self.notifications.auth_response, {
           // instanceID: self.config.instanceID,
           payloadReturn: response.statusCode,
           status: 'NOTOK',
-          message: result,
+          message: result
         })
       }
-    });
+    })
   },
-  callbackData: function(response) {
+  callbackData: function (response) {
     const self = this
-    let result = '';
+    let result = ''
 
-    response.on('error', function(e) {console.log('error', e)})
-    response.on('data', function (chunk) {result += chunk})
+    response.on('error', function (e) { console.log('error', e) })
+    response.on('data', function (chunk) { result += chunk })
     response.on('end', function () {
-      result = JSON.parse(result);
+      result = JSON.parse(result)
       if (response.statusCode == '200') {
         self.sendSocketNotification(self.notifications.data_response, {
           payloadReturn: result.body.devices,
-          status: 'OK',
+          status: 'OK'
         })
       } else if (response.statusCode == '403') {
         console.log('status code:', response.statusCode, '\n', result)
         self.sendSocketNotification(self.notifications.data_response, {
           payloadReturn: response.statusCode,
           status: 'INVALID_TOKEN',
-          message: result,
+          message: result
         })
       } else {
         console.log('status code:', response.statusCode, '\n', result)
         self.sendSocketNotification(self.notifications.data_response, {
           payloadReturn: response.statusCode,
           status: 'NOTOK',
-          message: result,
+          message: result
         })
       }
     })
   },
   socketNotificationReceived: function (notification, payload) {
     if (notification === this.notifications.auth) {
-      this.authenticate(payload);
+      this.authenticate(payload)
     } else if (notification === this.notifications.data) {
-      if (this.token === null){
+      if (this.token === null) {
         this.sendSocketNotification(this.notifications.data_response, {
           payloadReturn: 400,
           status: 'INVALID_TOKEN',
-          message: 'token not set',
+          message: 'token not set'
         })
       } else {
-        this.loadData(payload);
+        this.loadData(payload)
       }
     }
   }
