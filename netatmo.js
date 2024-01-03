45113b2b24c5f5aad4cf2d0a55b479992ca0714c
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2024-01-03 17:41:41 +0100

    fix: make en the fallback language (#321)

--- netatmo.js
+++ netatmo.js
@@ -419,9 +419,9 @@ Module.register('netatmo', {
   },
   getTranslations: function () {
     return {
+      en: 'l10n/en.json', // fallback language
       cs: 'l10n/cs.json',
       de: 'l10n/de.json',
-      en: 'l10n/en.json',
       fr: 'l10n/fr.json',
       hu: 'l10n/hu.json',
       nb: 'l10n/nb.json',
