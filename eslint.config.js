3419c87007e2d42fc08d6a1ae1fc751de3083e1e
Christopher Fenner <Christopher.Fenner@me.com>
2024-01-03 21:11:18 +0100

    add new config file

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+      rules: {
+          "comma-dangle": ["error", "always-multiline"],
+      },
+  },
+];
+
+// current config
+// env:
+//   browser: true
+//   es2022: true
+//   node: true
+//   jest: true
+// extends:
+//   - standard
+// parserOptions:
+//   sourceType: module
+//   ecmaVersion: 2022
+// globals:
+//   Module: true
+//   Log: true
+// rules:
+//   "comma-dangle": ["error", "always-multiline"]
